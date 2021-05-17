460762f6f7aec373f35c1d085d32b09bcb044c2a
soff <79422463+s-sofism@users.noreply.github.com>
2021-05-17 03:54:49 +0300

    Update main.h

--- laba_3/main.h
+++ laba_3/main.h
@@ -1,4 +1,4 @@
 int** array_malloc(int n, int m);
 void array_free(int **array, int n);
 void calculation(int **array, int n);
-int testing(int x, int y, int** array);
+int test(int x, int y, int** array);
